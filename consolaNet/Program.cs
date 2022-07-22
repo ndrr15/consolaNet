33c328a9d153aeaf6d018955b65c00a184e97a75
DavidRodriguez <ndrr15@gmail.com>
2022-07-22 17:39:17 -0500

    initial commit

---
+++ consolaNet/Program.cs
@@ -0,0 +1,13 @@
+﻿using Prueba1;
+using System;
+namespace consolaNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
